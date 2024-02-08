96f225c6765ef29db8140fec47662293a3688f63
Hope Shirley <77952833+hopeminyoso@users.noreply.github.com>
2024-02-08 19:00:29 +0300

    update error handler

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser"); // Import body-parser module
 const app = express();
 const dotenv = require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 
 dbConnect();
+
+// Use body-parser middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Use body-parser middleware to parse URL-encoded requests
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/api/user", authRouter);
+app.use(notFound);
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
 });
+
